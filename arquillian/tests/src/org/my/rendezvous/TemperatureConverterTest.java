29e6dbc8e7904fe011d7958f77c31f58a7b25b68
Paul Robinson <paul.robinson@redhat.com>
2011-11-24 17:25:21 +0000

    General tidy

--- arquillian/tests/src/org/my/rendezvous/TemperatureConverterTest.java
+++ arquillian/tests/src/org/my/rendezvous/TemperatureConverterTest.java
@@ -9,53 +9,38 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import javax.inject.Inject;
 
 @RunWith(Arquillian.class)
 public class TemperatureConverterTest
 {
-
     @Inject
     private TemperatureConverter converter;
-
 
     @Deployment
     public static JavaArchive createTestArchive()
     {
 
         return ShrinkWrap.create(JavaArchive.class, "test.jar")
-
                 .addClasses(TemperatureConverter.class)
-
                 .addAsManifestResource(
-
                         EmptyAsset.INSTANCE,
-
                         ArchivePaths.create("beans.xml"));
 
     }
 
-
     @Test
     public void testConvertToCelsius()
     {
-
         Assert.assertEquals(converter.convertToCelsius(32d), 0d);
-
         Assert.assertEquals(converter.convertToCelsius(212d), 100d);
-
     }
-
 
     @Test
     public void testConvertToFarenheit()
     {
-
         Assert.assertEquals(converter.convertToFarenheit(0d), 32d);
-
         Assert.assertEquals(converter.convertToFarenheit(100d), 212d);
-
     }
 
 }
